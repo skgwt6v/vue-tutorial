c273c5df5b0a93516e432ddc059069d9c3ce3c33
takaaki-sukegawa <takaaki-sukegawa@gmo-sc.com>
2018-10-24 10:33:01 +0900

    test

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import Frontpage from '@/components/Frontpage'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      // path: '/',
+      // name: 'HelloWorld',
+      // component: HelloWorld
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Frontpage',
+      component: Frontpage
     }
   ]
 })
